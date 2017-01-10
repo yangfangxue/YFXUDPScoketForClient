320ca46b5a893eb14c84e177db3817902ddf81f8
xue-ios <122092669@qq.com>
2017-01-10 13:13:48 +0800

    init
    第一次提交

--- YFX_UDPScoketForClient/ViewController.m
+++ YFX_UDPScoketForClient/ViewController.m
@@ -7,23 +7,42 @@
 //
 
 #import "ViewController.h"
+#import "GCDAsyncUdpSocket.h"
 
-@interface ViewController ()
-
+@interface ViewController ()<GCDAsyncUdpSocketDelegate>
+{
+    GCDAsyncUdpSocket *udpSocket;
+}
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
+    
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+     udpSocket = [[GCDAsyncUdpSocket alloc] initWithDelegate:self delegateQueue:dispatch_get_main_queue()];
+    //绑定端口号8000
+    [udpSocket bindToPort:8000 error:nil];
+    
+    NSError *error = nil;
+    //允许广播
+    [udpSocket enableBroadcast:YES error:&error];
+    //客户端本机IP地址172.20.10.3 加入广播
+    [udpSocket joinMulticastGroup:@"172.20.10.3" error:nil];
+    //开始准备接收数据
+    [udpSocket beginReceiving:nil];
 }
-
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
+    
+    //给服务端发送消息
+    //172.20.10.1为服务端的IP地址
+    //端口号要写成和客户端的端口号不一样 8099是服务端的端口号
+    [udpSocket sendData:[@"我是客户端" dataUsingEncoding:NSUTF8StringEncoding] toHost:@"172.20.10.1" port:8099 withTimeout:-1 tag:10];
 }
-
-
+//UDP 回调函数
+- (void)udpSocket:(GCDAsyncUdpSocket *)sock didReceiveData:(NSData *)data fromAddress:(NSData *)address withFilterContext:(id)filterContext {
+    //接收客户端消息
+    NSLog(@"ReceiveData = %@",[[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding]);
+}
 @end
